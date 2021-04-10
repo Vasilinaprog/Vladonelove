e8fa05b27c6bb85c4ebcafacbba180ac0870aa85
Vasilinaprog <71177581+Vasilinaprog@users.noreply.github.com>
2021-04-11 00:25:46 +0300

    Add files via upload

---
+++ Socialnet/fistpage.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+
+
+<html lang="ru" xml:lang="ru">
+<head>
+<title> ЕГЭ</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf8" />
+<link rel="stylesheet" type="text/css" href="css.css" media="screen" />
+</head>
+
+<body>
+
+<div id="container">
+
+
+	<div id="header"><div class="headtitle"> ЕГЭ</div></div>
+
+
+
+	<div id="menu">
+		<ul>
+			<li><a href="second_page.php" title="">ТЕОРИЯ</a></li>
+			<li><a href="last_page.php" title="">ТЕСТЫ</a></li>
+			<li><a id="is_active">ПОДГОТОВКА</a></li>
+		</ul>
+	</div>
+
+	
+
+	<div id="content">
+	
+		<div id="insidecontent">
+			
+			<h1>НАЧАЛО</h1>
+			<h2>НАЧАЛО</h2>
+			
+			<h3>Зачем? </h3>
+			<p>Самый массовый дополнительный предмет для сдачи ЕГЭ - обществознание. Сам предмет по своему характеру является комплексным, то есть объединяет в себе знания различных дисциплин: экономика, политология, право, социология и др. По этой причине сдать экзамен отнюдь не такая легкая задача, как может показаться изначально.Выбор учебного пособия играет значительную роль на пути к получению заветного высокого балла. Рассмотрим популярные руководства, уже зарекомендовавшие себя в этом отношении. </p>
+		</div>
+
+		
+	
+		<div style="clear: both;"></div>
+	
+		
+	</div>
+	
+	
+
+	<div id="footer">
+		<span>LAYOUT &copy; Vsamotoy</span>
+	</div>
+
+	
+</div>
+</body>
+</html>
